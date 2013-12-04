9f27082caa83bbd93288183a4a628c85d64d1bb4
Juan Carlos Condori Machicado <juanc.com@gmail.com>
2013-12-04 12:33:36 -0400

    first commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.2.13'
+
+gem 'pg'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  # gem 'therubyracer', :platforms => :ruby
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+group :development do
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'meta_request'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Runpa::Application.config.secret_token = '545fee46be73ac2d3eed31bc4e6836351856ff600a013055dafe81366fe36dc5c72bb40a694254419b6cde117cca3fb48c2422ddb62ee4230779b25279b8e4cd'
