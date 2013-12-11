28ef31e2bffd23b21c737ef0e9f51be5d47b22e2
Juan Carlos Condori Machicado <juanc.com@gmail.com>
2013-12-11 11:09:55 -0400

    adición de filtro de búsqueda

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,4 +1,12 @@
 ActiveAdmin.register User do
+  index do
+    column :email
+    column :current_sign_in_at
+    column :last_sign_in_at
+    column :sign_in_count
+    column :roles
+    default_actions
+  end
 
-
+  filter :email
 end
