b728c69db2fce0c7ebab6c280681f93f3fa17d07
Juan Carlos Condori Machicado <juanc.com@gmail.com>
2014-02-05 10:27:20 -0400

    adición de pasos registro 1

--- app/controllers/register_steps_controller.rb
+++ app/controllers/register_steps_controller.rb
@@ -1,9 +1,26 @@
 class RegisterStepsController < ApplicationController
   include Wicked::Wizard
-  steps :factors, :family, :partnerships,  :production
+  steps :family, :partnerships, :factors, :production
 
   def show
-    @register = Register.find(params[:register_id])
+    @register = Register.find params[:register_id]
     render_wizard
+  end
+  
+  def update
+    @register = Register.find params[:id] 
+    @user.update_attributes params[:register]
+    case step
+    when :family
+    when :partnerships
+    when :factors
+    when :production
+    end
+    render_wizard @user
+  end
+
+  def finish_wizard_path
+    @register = Register.find params[:id]
+    register_path(@register)
   end
 end
