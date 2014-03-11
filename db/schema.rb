55df509e99ebee920d207c09d3fa4b7a06cb3da7
Juan Carlos Condori Machicado <juanc.com@gmail.com>
2014-03-11 18:38:39 -0400

    changes in schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140219183024) do
+ActiveRecord::Schema.define(:version => 20140311145034) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "namespace"
@@ -318,5 +318,22 @@ ActiveRecord::Schema.define(:version => 20140219183024) do
   end
 
   add_index "users_roles", ["user_id", "role_id"], :name => "index_users_roles_on_user_id_and_role_id"
+
+  create_table "versions", :force => true do |t|
+    t.string   "item_type",      :null => false
+    t.integer  "item_id",        :null => false
+    t.string   "event",          :null => false
+    t.string   "whodunnit"
+    t.text     "object"
+    t.datetime "created_at"
+    t.text     "object_changes"
+    t.integer  "parent_id"
+    t.string   "parent_type"
+    t.integer  "register_id"
+  end
+
+  add_index "versions", ["item_type", "item_id"], :name => "index_versions_on_item_type_and_item_id"
+  add_index "versions", ["parent_id"], :name => "index_versions_on_parent_id"
+  add_index "versions", ["register_id"], :name => "index_versions_on_register_id"
 
 end
