86440cd2414c261396ec9e2d0e71bc0af8d0fbd1
Juan Carlos Condori Machicado <juanc.com@gmail.com>
2014-04-04 09:56:05 -0400

    cambio de nombre a variables configurables de formulario

--- app/admin/economic_activity.rb
+++ app/admin/economic_activity.rb
@@ -1,4 +1,4 @@
 #encoding: UTF-8
 ActiveAdmin.register EconomicActivity do
-  menu parent: 'Administración Varibles Configurables'  
+  menu parent: 'Administración de Variables del Formulario' 
 end
--- app/admin/education.rb
+++ app/admin/education.rb
@@ -1,4 +1,4 @@
 #encoding: UTF-8
 ActiveAdmin.register Education do
-  menu parent: 'Administración Varibles Configurables'
+  menu parent: 'Administración de Variables del Formulario'
 end
