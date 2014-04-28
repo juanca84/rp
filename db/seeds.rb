589f1bdca7a094de7d7d26e61a656b413990182d
Juan Carlos Condori Machicado <juanc.com@gmail.com>
2014-04-28 15:49:05 -0400

    add initializers

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,6 @@ if !admin.new_record? && admin.add_role("administrador")
   puts 'administrador creado! email -> runpa.mdryt@gmail.com y password -> runpa1234'
 end
 
-
 puts "inicializando estados civiles"
 civil_status = 
   [
@@ -43,6 +42,63 @@ entities =
     { name: 'OAP- MDRyT' }
   ]
 entities.each do |cs|
-  entity = { code: cs[:code], name: cs[:name] }
-  en = CivilStatus.find_or_create_by_code_and_name(entity) 
+  entity = { name: cs[:name] }
+  en = RunpaModule.where(name: cs[:name]).first_or_create
+end
+
+puts "inicializando actividades economicas"
+economic_activities = 
+  [
+    { code: '9', name: 'OTROS' },
+    { code: '8', name: 'PESCA' },
+    { code: '7', name: 'CAZA' },
+    { code: '6', name: 'EXTRACCIÓN O RECOLECCIÓN DE ESPECIES NO MADERABLES' },
+    { code: '5', name: 'FORESTAL' },
+    { code: '4', name: 'APICULTURA' },
+    { code: '3', name: 'AVICULTURA' },
+    { code: '2', name: 'GANADERÍA' },
+    { code: '1', name: 'AGRICULTURA' }
+  ]
+economic_activities.each do |cs|
+  economic_activity = { code: cs[:code], name: cs[:name] }
+  en = EconomicActivity.find_or_create_by_code_and_name(economic_activity) 
+end
+
+puts "inicializando escolaridades"
+educations = 
+  [
+    { code: '7', name: 'UNIVERSITARIO' },
+    { code: '6', name: 'NORMAL(SUPERIOR)' },
+    { code: '5', name: 'TÉCNICO (MEDIO, SUPERIOR)' },
+    { code: '4', name: 'SECUNDARIA' },
+    { code: '3', name: 'PRIMARIA' },
+    { code: '2', name: 'CURSO DE ALFABETIZACIÓN' },
+    { code: '1', name: 'NINGUNO' }
+  ]
+educations.each do |cs|
+  education = { code: cs[:code], name: cs[:name] }
+  en = Education.find_or_create_by_code_and_name(education) 
+end
+
+puts "inicializando tipos de identificación"
+identifications = 
+  [
+    { code: '02', name: 'PASAPORTE' },
+    { code: '01', name: 'CÉDULA DE IDENTIDAD' }
+  ]
+identifications.each do |cs|
+  identification = { code: cs[:code], name: cs[:name] }
+  en = TypeOfIdentification.find_or_create_by_code_and_name(identification) 
+end
+
+puts "inicializando tipos de tipos de residencias"
+type_residences = 
+  [
+    { code: '03', name: 'COMUNIDAD/LOCALIDAD/ESTANCIA/RANCHO' },
+    { code: '02', name: 'CENTRO POBLADO(MUNICIPIO)' },
+    { code: '01', name: 'CAPITAL DE DEPARTAMENTO(CIUDAD)' }
+  ]
+type_residences.each do |cs|
+  type_residence = { code: cs[:code], name: cs[:name] }
+  en = TypeResidence.find_or_create_by_code_and_name(type_residence) 
 end
