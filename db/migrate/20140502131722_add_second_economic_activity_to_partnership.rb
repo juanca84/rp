0d590c597e250f387dc8e777852cb9d1f7a744e0
Juan Carlos Condori Machicado <juanc.com@gmail.com>
2014-05-05 15:48:44 -0400

    fix migration partneships

--- db/migrate/20140502131722_add_second_economic_activity_to_partnership.rb
+++ db/migrate/20140502131722_add_second_economic_activity_to_partnership.rb
@@ -1,5 +1,5 @@
 class AddSecondEconomicActivityToPartnership < ActiveRecord::Migration
-  def change
+  def up
     add_column :partnerships, :economic_activity_1_2_id, :integer
     add_column :partnerships, :first_entry_1_2, :string
     add_column :partnerships, :second_entry_1_2, :string
@@ -13,11 +13,24 @@ class AddSecondEconomicActivityToPartnership < ActiveRecord::Migration
     rename_column :partnerships, :first_entry_2, :first_entry_2_1
     rename_column :partnerships, :second_entry_2, :second_entry_2_1
 
-    add_index :partnerships, :economic_activity_1_1_id
     add_index :partnerships, :economic_activity_1_2_id
-    add_index :partnerships, :economic_activity_2_1_id
     add_index :partnerships, :economic_activity_2_2_id
-    remove_index :partnerships, :economic_activity_1_id
-    remove_index :partnerships, :economic_activity_2_id
+  end
+
+  def down
+    remove_column :partnerships, :economic_activity_1_2_id
+    remove_column :partnerships, :first_entry_1_2
+    remove_column :partnerships, :second_entry_1_2
+    remove_column :partnerships, :economic_activity_2_2_id
+    remove_column :partnerships, :first_entry_2_2
+    remove_column :partnerships, :second_entry_2_2
+
+    rename_column :partnerships, :economic_activity_1_1_id, :economic_activity_1_id
+    rename_column :partnerships, :first_entry_1_1, :first_entry_1
+    rename_column :partnerships, :second_entry_1_1, :second_entry_1
+    rename_column :partnerships, :economic_activity_2_1_id, :economic_activity_2_id
+    rename_column :partnerships, :first_entry_2_1, :first_entry_2
+    rename_column :partnerships, :second_entry_2_1, :second_entry_2
+
   end
 end
